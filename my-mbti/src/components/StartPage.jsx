5f4136670ae9537b85dbd49c19295b78e5d65358
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-05-05 22:46:15 -0700

    css overflow

--- my-mbti/src/components/StartPage.jsx
+++ my-mbti/src/components/StartPage.jsx
@@ -2,10 +2,12 @@ import { Container, Row, Col, Button, Card } from 'react-bootstrap'
 
 export default function StartPage({setCurrentPage}) {
     return (
-        <Container   fluid
+        <Container   
+            fluid
+            style={{ overflow: 'hidden' }}
             className="vh-100 d-flex flex-column justify-content-center align-items-center">
             {/* background image via CSS on body or a wrapper div */}
-            <Row className="justify-content-center mb-4">
+            <Row className="justify-content-center d-flex flex-row mb-4">
                 <Col xs={12} md={8}>
                 <h1 className="text-center">Reincarnated As A Jobless Kitty Cat In Another World?</h1>
                 </Col>
@@ -13,16 +15,16 @@ export default function StartPage({setCurrentPage}) {
 
             <Row className="justify-content-center">
                 <Col xs="auto">
-                <Card style={{ width: '18rem' }}>
-                    <Card.Body className="text-center">
-                    <Card.Text className="fs-4 fs-md-2 mb-4">
-                        Unlock your archetype personality in 5 minutes and discover your unique strengths!
-                    </Card.Text>
-                    <Button size="lg" onClick={() => setCurrentPage('quiz')}>
-                        Discover Your Kitty Self
-                    </Button>
-                    </Card.Body>
-                </Card>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body className="text-center">
+                        <Card.Text className="fs-4 fs-md-2 mb-4">
+                            Unlock your archetype personality in 5 minutes and discover your unique strengths!
+                        </Card.Text>
+                        <Button size="lg" onClick={() => setCurrentPage('quiz')}>
+                            Discover Your Kitty Self
+                        </Button>
+                        </Card.Body>
+                    </Card>
                 </Col>
             </Row>
         </Container>
